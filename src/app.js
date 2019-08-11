6208c197a9da011bca9cf09d37f27cb7d25bed32
arshdeep1535@gmail.com <arshdeep1535@gmail.com>
2019-08-12 02:38:59 +0530

    formatting amount and date

--- src/app.js
+++ src/app.js
@@ -3,24 +3,10 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';  
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 const store = configureStore();
-console.log(store);
-console.log(addExpense);
-
-const expenseOne = store.dispatch(addExpense({description:'rent',amount:2001,createdAt:-1233 }));
-const expenseTwo = store.dispatch(addExpense({description:'coffee',amount:2000}));
-const expenseThree = store.dispatch(addExpense({description:'electricity bill',amount:201,createdAt:33 }));
-const expenseFour = store.dispatch(addExpense({description:'water bill',amount:2009,createdAt:1200}));  
-store.dispatch(setTextFilter(''));
-const state = store.getState();
-const expenses = getVisibleExpenses(state.expenses,state.filters);
-console.log(expenses); 
 
 const jsx = (
     <Provider store={store}>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseItem = (props) => (
     <div>
         <Link to={`/edit/${props.id}`}>
             <h3>{props.description}</h3>
         </Link>
-        <p>{props.amount} - {props.createdAt}</p>
+        <p>{numeral(props.amount/100).format('$0,0.00')} - {moment(props.createdAt).format('Do MMMM,YYYY')}</p>
         
     </div>
 );
