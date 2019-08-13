719c652a5f059f33806f9d6d23d46d6e526cb27f
arshdeep1535@gmail.com <arshdeep1535@gmail.com>
2019-08-14 00:17:54 +0530

    added update and remove

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import {editExpense} from '../actions/expenses';
-import { removeExpense } from '../actions/expenses';
+import { startEditExpense} from '../actions/expenses';
+import { startRemoveExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
     return  (
@@ -10,12 +10,12 @@ const EditExpensePage = (props) => {
             <ExpenseForm
                 expense = {props.expense}
                 onSubmit = {(expense) => {
-                    props.dispatch(editExpense(props.expense.id,expense));
+                    props.dispatch(startEditExpense(props.expense.id,expense));
                     props.history.push('/');
                 }}
             />
             <button onClick={ () => {
-                props.dispatch(removeExpense({id: props.expense.id}));
+                props.dispatch(startRemoveExpense({id: props.expense.id}));
                 props.history.push('/');
             }}>Remove</button>
         </div>
